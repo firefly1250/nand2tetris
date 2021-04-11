ca8432d7165e2b22d8fbad7130effe28d70a492a
Haruto <ynhrt1250@gmail.com>
2021-04-12 00:02:30 +0900

    finished Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,63 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// R2 = 0
+
+// if R1 & 0b0001
+// R1 += R0
+
+// bit1shift = R0 *2
+// if R1 & 0b0010
+//   R2 += bit1shift
+
+// bit2shift = bit1shift * 2
+// if R1 & 0b0100
+//   R2 += bit1shift
+
+// ...
+
+//@3
+//D=A
+//@R0
+//M=D
+//
+//@1
+//D=A
+//@R1
+//M=D
+
+@R2
+M=0
+
+@BIT
+M=1
+
+@R0
+D=M
+@SHIFT
+M=D
+
+(LOOP)
+@R1
+D=M
+@BIT
+D=D&M
+@ELSE
+D;JEQ
+@SHIFT
+D=M
+@R2
+M=D+M
+(ELSE)
+@BIT
+D=M
+MD=D+M
+@END
+D;JEQ
+@SHIFT
+D=M
+M=D+M
+@LOOP
+0;JMP
+(END)
