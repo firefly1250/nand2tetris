48089bd4e620ea8d87b9036698c038a6edadd3b7
Haruto <ynhrt1250@gmail.com>
2021-04-18 19:14:56 +0900

    finished chap.4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,47 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+D=A
+@8192
+D=D+A
+@SCREEN_END
+M=D
+
+(LOOP)
+@KBD
+D=M
+@WHITE
+D;JEQ
+
+(BLACK)
+@COLOR
+M=-1
+@COLOR_END
+0;JMP
+
+(WHITE)
+@COLOR
+M=0
+
+(COLOR_END)
+
+@SCREEN
+D=A
+@COUNTER
+M=D
+(FILL)
+@COLOR
+D=M
+@COUNTER
+A=M
+M=D
+@COUNTER
+MD=M+1
+@SCREEN_END
+D=M-D
+@FILL
+D;JGT
+
+@LOOP
+0;JMP
